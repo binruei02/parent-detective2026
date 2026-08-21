a03272a1eb2c3c92c9391c8b1580e2b9588aae9a
binruei02 <binruei@gmail.com>
2026-08-21 20:52:03 +0800

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,15 @@
+/**
+ * 資訊識讀偵探事務所 - Firebase 設定檔
+ * 專案代碼：parent-detective-2026
+ * 即時資料庫地區：asia-southeast1 (新加坡)
+ */
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDEma13-AQErZD9t7Pw09-0bGdV6bGWFw8",
+  authDomain: "parent-detective-2026.firebaseapp.com",
+  databaseURL: "https://parent-detective-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "parent-detective-2026",
+  storageBucket: "parent-detective-2026.firebasestorage.app",
+  messagingSenderId: "139579646361",
+  appId: "1:139579646361:web:0c5d9fbe88cb6ca0dc7308"
+};
